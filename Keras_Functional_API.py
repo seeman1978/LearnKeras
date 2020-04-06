bb890b04c8b3ffc54d9b42811acbc171ee8a169e
wangqiang <seeman1978@163.com>
2020-04-06 19:54:16 +0800

    keras functional api Tensorflow 2.0

--- Keras_Functional_API.py
+++ Keras_Functional_API.py
@@ -47,3 +47,25 @@ if __name__ == "__main__":
     # Recreate the exact same model purely from the file:
     model2 = keras.models.load_model('my_model')
 
+    #Use the same graph of layers to define multiple models
+    encoder_input = keras.Input(shape=(28, 28, 1), name='img')
+    x = layers.Conv2D(16, 3, activation='relu')(encoder_input)
+    x = layers.Conv2D(32, 3, activation='relu')(x)
+    x = layers.MaxPooling2D(3)(x)
+    x = layers.Conv2D(32, 3, activation='relu')(x)
+    x = layers.Conv2D(16, 3, activation='relu')(x)
+    encoder_output = layers.GlobalMaxPooling2D()(x)
+
+    encoder = keras.Model(encoder_input, encoder_output, name='encoder')
+    encoder.summary()
+
+    x = layers.Reshape((4, 4, 1))(encoder_output)
+    x = layers.Conv2DTranspose(16, 3, activation='relu')(x)
+    x = layers.Conv2DTranspose(32, 3, activation='relu')(x)
+    x = layers.UpSampling2D(3)(x)
+    x = layers.Conv2DTranspose(16, 3, activation='relu')(x)
+    decoder_output = layers.Conv2DTranspose(1, 3, activation='relu')(x)
+
+    autoencoder = keras.Model(encoder_input, decoder_output, name='autoencoder')
+    autoencoder.summary()
+    #The reverse of a Conv2D layer is a Conv2DTranspose layer, and the reverse of a MaxPooling2D layer is an UpSampling2D layer.
