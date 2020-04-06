320695fc7205adfa68f165875b9c4c2e20de0d80
wangqiang <seeman1978@163.com>
2020-04-06 20:42:55 +0800

    Multiple input and output Tensorflow 2.0

---
+++ Multiple_Inputs_AndOutputs.py
@@ -0,0 +1,62 @@
+"""
+Models with multiple inputs and outputs.
+"""
+import numpy as np
+
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras import layers
+
+tf.keras.backend.clear_session()  # For easy reset of notebook state.
+
+if __name__ == "__main__":
+    num_tags = 12  # Number of unique issue tags
+    num_words = 10000  # Size of vocabulary obtained when preprocessing text data
+    num_departments = 4  # Number of departments for predictions
+
+    title_input = keras.Input(shape=(None,), name='title')  # Variable-length sequence of ints
+    body_input = keras.Input(shape=(None,), name='body')  # Variable-length sequence of ints
+    tags_input = keras.Input(shape=(num_tags,), name='tags')  # Binary vectors of size `num_tags`
+
+    # Embed each word in the title into a 64-dimensional vector
+    title_features = layers.Embedding(num_words, 64)(title_input)
+    # Embed each word in the text into a 64-dimensional vector
+    body_features = layers.Embedding(num_words, 64)(body_input)
+
+    # Reduce sequence of embedded words in the title into a single 128-dimensional vector
+    title_features = layers.LSTM(128)(title_features)
+    # Reduce sequence of embedded words in the body into a single 32-dimensional vector
+    body_features = layers.LSTM(32)(body_features)
+
+    # Merge all available features into a single large vector via concatenation
+    x = layers.concatenate([title_features, body_features, tags_input])
+
+    # Stick a logistic regression for priority prediction on top of the features
+    priority_pred = layers.Dense(1, name='priority')(x)
+    # Stick a department classifier on top of the features
+    department_pred = layers.Dense(num_departments, name='department')(x)
+
+    # Instantiate an end-to-end model predicting both priority and department
+    model = keras.Model(inputs=[title_input, body_input, tags_input],
+                        outputs=[priority_pred, department_pred])
+    keras.utils.plot_model(model, 'multi_input_and_output_model.png', show_shapes=True)
+
+    model.compile(optimizer=keras.optimizers.RMSprop(1e-3),
+                  loss={'priority': keras.losses.BinaryCrossentropy(from_logits=True),
+                        'department': keras.losses.CategoricalCrossentropy(from_logits=True)},
+                  loss_weights=[1., 0.2])
+
+    # Dummy input data
+    title_data = np.random.randint(num_words, size=(1280, 10))
+    body_data = np.random.randint(num_words, size=(1280, 100))
+    tags_data = np.random.randint(2, size=(1280, num_tags)).astype('float32')
+
+    # Dummy target data
+    priority_targets = np.random.random(size=(1280, 1))
+    dept_targets = np.random.randint(2, size=(1280, num_departments))
+
+    model.fit({'title': title_data, 'body': body_data, 'tags': tags_data},
+              {'priority': priority_targets, 'department': dept_targets},
+              epochs=2,
+              batch_size=32)
