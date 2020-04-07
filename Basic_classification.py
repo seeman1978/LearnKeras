5f7b886642ca97c736ba84e2aadb6961b63dcb5e
wangqiang <seeman1978@163.com>
2020-04-07 20:37:01 +0800

    Basic classification: Classify images of clothing Tensorflow 2.0

---
+++ Basic_classification.py
@@ -0,0 +1,38 @@
+# TensorFlow and tf.keras
+import tensorflow as tf
+from tensorflow import keras
+
+# Helper libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+if __name__ == "__main__":
+    fashion_mnist = keras.datasets.fashion_mnist
+
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    #process these data
+    train_images = train_images / 255.0
+
+    test_images = test_images / 255.0
+
+    #Build the model
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation='relu'),
+        keras.layers.Dense(10)
+    ])
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+    #Train the model
+    model.fit(train_images, train_labels, epochs=10)
+
+    #Evaluate accuracy
+    test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+
+    print('\nTest accuracy:', test_acc)
