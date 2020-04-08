d8ba7929391bb270713ffa9f047fc44326bf16b7
wangqiang <seeman1978@163.com>
2020-04-08 21:31:06 +0800

    Basic classification: Classify images of clothing Tensorflow 2.0

--- Basic_classification.py
+++ Basic_classification.py
@@ -7,6 +7,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 print(tf.__version__)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+
 if __name__ == "__main__":
     fashion_mnist = keras.datasets.fashion_mnist
 
@@ -36,3 +49,13 @@ if __name__ == "__main__":
     test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
     print('\nTest accuracy:', test_acc)
+
+    probability_model = tf.keras.Sequential([model,
+                                             tf.keras.layers.Softmax()])
+
+    predictions = probability_model.predict(test_images)
+
+    np.argmax(predictions[0])
+
+    plot_value_array(1, predictions[0], test_labels)
+    _ = plt.xticks(range(10), class_names, rotation=45)
