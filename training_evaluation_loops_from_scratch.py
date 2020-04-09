d50970f8f76937d9cf4f596a3b9bb96681684b6d
wangqiang <seeman1978@163.com>
2020-04-09 15:35:20 +0800

    custom train and evaluation loops from scratch Tensorflow 2.0

---
+++ training_evaluation_loops_from_scratch.py
@@ -0,0 +1,103 @@
+import tensorflow as tf
+
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+
+if __name__ == "__main__":
+    # load data
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+    # Preprocess the data (these are Numpy arrays)
+    x_train = x_train.reshape(60000, 784).astype('float32') / 255
+    x_test = x_test.reshape(10000, 784).astype('float32') / 255
+
+    y_train = y_train.astype('float32')
+    y_test = y_test.astype('float32')
+
+    # Reserve 10,000 samples for validation
+    x_val = x_train[-10000:]
+    y_val = y_train[-10000:]
+    x_train = x_train[:-10000]
+    y_train = y_train[:-10000]
+
+    # First, let's create a training Dataset instance.
+    # For the sake of our example, we'll use the same MNIST data as before.
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    # Shuffle and slice the dataset.
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(64)
+
+    # Prepare the validation dataset
+    val_dataset = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    val_dataset = val_dataset.batch(64)
+
+    # Now we get a test dataset.
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    test_dataset = test_dataset.batch(64)
+
+    # Get the model.
+    inputs = keras.Input(shape=(784,), name='digits')
+    x = layers.Dense(64, activation='relu', name='dense_1')(inputs)
+    x = layers.Dense(64, activation='relu', name='dense_2')(x)
+    outputs = layers.Dense(10, name='predictions')(x)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+
+    # Instantiate an optimizer.
+    optimizer = keras.optimizers.SGD(learning_rate=1e-3)
+    # Instantiate a loss function.
+    loss_fn = keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    # Prepare the metrics.
+    train_acc_metric = keras.metrics.SparseCategoricalAccuracy()
+    val_acc_metric = keras.metrics.SparseCategoricalAccuracy()
+
+    epochs = 3
+    for epoch in range(epochs):
+        print('Start of epoch %d' % (epoch,))
+
+        # Iterate over the batches of the dataset.
+        for step, (x_batch_train, y_batch_train) in enumerate(train_dataset):
+
+            # Open a GradientTape to record the operations run
+            # during the forward pass, which enables autodifferentiation.
+            with tf.GradientTape() as tape:
+
+                # Run the forward pass of the layer.
+                # The operations that the layer applies
+                # to its inputs are going to be recorded
+                # on the GradientTape.
+                logits = model(x_batch_train, training=True)  # Logits for this minibatch
+
+                # Compute the loss value for this minibatch.
+                loss_value = loss_fn(y_batch_train, logits)
+
+            # Use the gradient tape to automatically retrieve
+            # the gradients of the trainable variables with respect to the loss.
+            grads = tape.gradient(loss_value, model.trainable_weights)
+
+            # Run one step of gradient descent by updating
+            # the value of the variables to minimize the loss.
+            optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+            # Update training metric.
+            train_acc_metric(y_batch_train, logits)
+
+            # Log every 200 batches.
+            if step % 200 == 0:
+                print('Training loss (for one batch) at step %s: %s' % (step, float(loss_value)))
+                print('Seen so far: %s samples' % ((step + 1) * 64))
+
+        # Display metrics at the end of each epoch.
+        train_acc = train_acc_metric.result()
+        print('Training acc over epoch: %s' % (float(train_acc),))
+        # Reset training metrics at the end of each epoch
+        train_acc_metric.reset_states()
+
+        # Run a validation loop at the end of each epoch.
+        for x_batch_val, y_batch_val in val_dataset:
+            val_logits = model(x_batch_val)
+            # Update val metrics
+            val_acc_metric(y_batch_val, val_logits)
+        val_acc = val_acc_metric.result()
+        val_acc_metric.reset_states()
+        print('Validation acc: %s' % (float(val_acc),))
